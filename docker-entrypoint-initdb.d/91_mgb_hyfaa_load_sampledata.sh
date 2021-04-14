2b93b04c6b9539d7ffa419bc81b51bec28ecb149
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2021-04-14 20:11:42 +0200

    Fix samples & samples loading

--- docker-entrypoint-initdb.d/91_mgb_hyfaa_load_sampledata.sh
+++ docker-entrypoint-initdb.d/91_mgb_hyfaa_load_sampledata.sh
@@ -4,8 +4,7 @@ set -e
 
 if [[ "$WITH_SAMPLE_DATA" == "yes" ]]; then
   echo ... Load sample data ...
-  psql -d $POSTGRES_DB -U $POSTGRES_USER -c "COPY hyfaa.data_assimilated FROM PROGRAM 'gunzip -c /docker-entrypoint-initdb.d/data/data_assimilated_sample.sql.gz';"
-  psql -d $POSTGRES_DB -U $POSTGRES_USER -c "COPY hyfaa.data_assimilated FROM PROGRAM 'gunzip -c /docker-entrypoint-initdb.d/data/data_assimilated_stations_sample.sql.gz';"
-  psql -d $POSTGRES_DB -U $POSTGRES_USER -c "COPY hyfaa.data_mgbstandard FROM PROGRAM 'gunzip -c /docker-entrypoint-initdb.d/data/data_mgbstandard_stations_sample.sql.gz';"
-  psql -d $POSTGRES_DB -U $POSTGRES_USER -c "COPY hyfaa.data_forecast FROM PROGRAM 'gunzip -c /docker-entrypoint-initdb.d/data/data_forecast_stations_sample.sql.gz';"
+  psql -d $POSTGRES_DB -U $POSTGRES_USER -c "COPY hyfaa.data_assimilated FROM PROGRAM 'gunzip -c /docker-entrypoint-initdb.d/data/data_assimilated_sample.csv.gz' delimiter ',' CSV HEADER;"
+  psql -d $POSTGRES_DB -U $POSTGRES_USER -c "COPY hyfaa.data_mgbstandard FROM PROGRAM 'gunzip -c /docker-entrypoint-initdb.d/data/data_mgbstandard_stations_sample.csv.gz' delimiter ',' CSV HEADER;"
+  psql -d $POSTGRES_DB -U $POSTGRES_USER -c "COPY hyfaa.data_forecast FROM PROGRAM 'gunzip -c /docker-entrypoint-initdb.d/data/data_forecast_stations_sample.csv.gz' delimiter ',' CSV HEADER;"
 fi
