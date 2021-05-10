370755b7c356d46b273dbafa42adc939b8639a2e
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2021-05-10 12:27:01 +0200

    Harmonized floating mean calculation. Optimise the views.

--- docker-entrypoint-initdb.d/25_mgb_hyfaa_views.sql
+++ docker-entrypoint-initdb.d/25_mgb_hyfaa_views.sql
@@ -2,8 +2,9 @@
 -- Aggregate the last 15d values in a json field
 -----------------------------------------------------
 
+-----------------------------------------------------
 -- on assimilated data
-CREATE OR REPLACE VIEW hyfaa.data_assimilated_aggregate_json
+CREATE MATERIALIZED VIEW hyfaa.data_assimilated_with_floating_avg_and_anomaly
  AS
  WITH has_average AS (
 	WITH has_mean_over_ddoy AS (SELECT * FROM (
@@ -24,15 +25,35 @@ CREATE OR REPLACE VIEW hyfaa.data_assimilated_aggregate_json
 	FROM has_mean_over_ddoy
 )
 SELECT cell_id,
+       "date",
+       flow_median AS flow,
+       flow_mad,
+       average AS expected,
+       CASE WHEN average = 0 THEN null ELSE (100 * (flow_median - average) / average)::numeric END AS flow_anomaly
+FROM has_average
+ORDER BY cell_id;
+
+ALTER TABLE hyfaa.data_assimilated_with_floating_avg_and_anomaly
+    OWNER TO postgres;
+
+COMMENT ON MATERIALIZED VIEW hyfaa.data_assimilated_with_floating_avg_and_anomaly IS
+    'Keep only the flow median and flow_mad. Compute a floating average for each minibasin
+    (average over the day +/- 15days, but spanning over the years.)
+    Use it to compute the anomaly.';
+
+
+CREATE OR REPLACE VIEW hyfaa.data_assimilated_aggregate_json
+ AS
+SELECT cell_id,
        json_agg(
           json_build_object(
       		'date', date,
-      		'flow', ROUND(flow_median),
-      		'flow_anomaly', CASE WHEN average = 0 THEN null ELSE ROUND((100 * (flow_median - average) / average)::numeric) END
+      		'flow', ROUND(flow),
+      		'flow_anomaly', ROUND(flow_anomaly)
       		)
 	        ORDER BY "date" DESC
         ) AS values
-FROM has_average
+FROM hyfaa.data_assimilated_with_floating_avg_and_anomaly
 WHERE "date" IN (SELECT "date" from hyfaa.data_assimilated
 						GROUP BY "date" ORDER BY "date" DESC LIMIT 15)
 GROUP BY cell_id
@@ -42,15 +63,16 @@ ALTER TABLE hyfaa.data_assimilated_aggregate_json
     OWNER TO postgres;
 
 COMMENT ON  VIEW hyfaa.data_assimilated_aggregate_json IS
-    'Keep only the flow median. Compute a floating average for each minibasin
-    (average over the day +/- 15days, but spanning over the years.)
-    Use it to compute the anomaly. Round the values.
+    'Round the values.
     Aggregate the results for the n last days into a json field';
 
+
+
+-----------------------------------------------------
 -- on mgbstandard data
-CREATE OR REPLACE VIEW hyfaa.data_mgbstandard_aggregate_json
+CREATE MATERIALIZED VIEW hyfaa.data_mgbstandard_with_floating_avg_and_anomaly
  AS
-WITH has_average AS (
+ WITH has_average AS (
 	WITH has_mean_over_ddoy AS (SELECT * FROM (
 		( SELECT *, date_part('doy', "date") AS ddoy FROM hyfaa.data_mgbstandard WHERE "date" >= now() - '1 year'::interval ) AS d
 		LEFT JOIN
@@ -69,16 +91,35 @@ WITH has_average AS (
 	FROM has_mean_over_ddoy
 )
 SELECT cell_id,
+       "date",
+       flow_mean AS flow,
+       average AS expected,
+       CASE WHEN average = 0 THEN null ELSE (100 * (flow_mean - average) / average)::numeric END AS flow_anomaly
+FROM has_average
+ORDER BY cell_id;
+
+ALTER TABLE hyfaa.data_mgbstandard_with_floating_avg_and_anomaly
+    OWNER TO postgres;
+
+COMMENT ON MATERIALIZED VIEW hyfaa.data_mgbstandard_with_floating_avg_and_anomaly IS
+    'Keep only the flow mean. Compute a floating average for each minibasin
+    (average over the day +/- 15days, but spanning over the years.)
+    Use it to compute the anomaly.';
+
+
+CREATE OR REPLACE VIEW hyfaa.data_mgbstandard_aggregate_json
+ AS
+SELECT cell_id,
        json_agg(
           json_build_object(
       		'date', date,
-      		'flow', ROUND(flow_mean),
-      		'flow_anomaly', CASE WHEN average = 0 THEN null ELSE ROUND((100 * (flow_mean - average) / average)::numeric) END
+      		'flow', ROUND(flow),
+      		'flow_anomaly', ROUND(flow_anomaly)
       		)
 	        ORDER BY "date" DESC
         ) AS values
-FROM has_average
-WHERE "date" IN (SELECT "date" from hyfaa.data_assimilated
+FROM hyfaa.data_mgbstandard_with_floating_avg_and_anomaly
+WHERE "date" IN (SELECT "date" from hyfaa.data_mgbstandard
 						GROUP BY "date" ORDER BY "date" DESC LIMIT 15)
 GROUP BY cell_id
 ORDER BY cell_id;
@@ -87,12 +128,8 @@ ALTER TABLE hyfaa.data_mgbstandard_aggregate_json
     OWNER TO postgres;
 
 COMMENT ON  VIEW hyfaa.data_mgbstandard_aggregate_json IS
-    'Keep only the flow mean. Compute a floating average for each minibasin
-    (average over the day +/- 15days, but spanning over the years.)
-    Use it to compute the anomaly. Round the values.
+    'Round the values.
     Aggregate the results for the n last days into a json field';
-
-
 
 
 -----------------------------------------------------
@@ -104,24 +141,14 @@ CREATE OR REPLACE FUNCTION hyfaa.get_assimilated_values_for_minibasin(mini integ
 $$
 DECLARE jsonarray json;
 BEGIN
-		WITH tbl AS (WITH expect AS (SELECT cell_id, date_part('doy', "date") AS ddoy, avg(flow_median) AS expected
-			FROM hyfaa.data_assimilated
-			GROUP BY cell_id, date_part('doy', "date")
-			),
-			d_with_assim AS (
-				  SELECT cell_id, "date", flow_median, flow_mad, date_part('doy', "date") AS ddoy
-				  FROM hyfaa.data_assimilated
-				  WHERE "date" > now()-timeinterval::interval
-			)
-
-			SELECT 	 d_with_assim."date",
-					 ROUND(d_with_assim.flow_median::numeric) AS flow,
-					 ROUND(d_with_assim.flow_mad::numeric,1) AS flow_mad,
-					 ROUND(expect.expected::numeric) AS expected
-			FROM  d_with_assim LEFT JOIN expect
-								USING(ddoy, cell_id)
-			WHERE cell_id=mini
-			ORDER BY d_with_assim."date" DESC
+		WITH tbl AS (
+		    SELECT "date",
+		           ROUND(flow::numeric) AS flow,
+		           ROUND(flow_mad::numeric) AS flow_mad,
+		           ROUND(expected::numeric) AS expected
+		    FROM hyfaa.data_assimilated_with_floating_avg_and_anomaly
+		    WHERE cell_id=mini AND "date" > now()-timeinterval::interval
+		    ORDER BY "date" DESC
 		)
 		SELECT array_to_json(array_agg(row_to_json(tbl))) FROM tbl INTO jsonarray;
 		return jsonarray;
@@ -129,7 +156,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 COMMENT ON FUNCTION hyfaa.get_assimilated_values_for_minibasin(integer, character varying)
-    IS 'Get assimilated data as aggregated json array. Adds a mean value over all the value existing at the same day of the year (mean over the years), called ''expected''';
+    IS 'Get assimilated data as aggregated json array, rounding the values to closest integer';
 
 
 CREATE OR REPLACE FUNCTION hyfaa.get_mgbstandard_values_for_minibasin(mini integer, timeinterval varchar default '1 year')
@@ -139,8 +166,9 @@ DECLARE jsonarray json;
         BEGIN
         WITH tbl AS (
             SELECT 	"date",
-                    ROUND(flow_mean::numeric) AS flow
-            FROM  hyfaa.data_mgbstandard
+		           ROUND(flow::numeric) AS flow,
+		           ROUND(expected::numeric) AS expected
+            FROM  hyfaa.data_mgbstandard_with_floating_avg_and_anomaly
             WHERE cell_id=mini AND "date" > now()-timeinterval::interval
 			ORDER BY "date" DESC
         )
@@ -150,7 +178,8 @@ END;
 $$ LANGUAGE plpgsql;
 
 COMMENT ON FUNCTION hyfaa.get_mgbstandard_values_for_minibasin(integer, character varying)
-    IS 'Get mgbstandard data as aggregated json array (array of {date, flow_mean} values) ';
+    IS 'Get mgbstandard data as aggregated json array (array of {date, flow_mean, floating mean as "expected"} values),
+        rounding the values to closest integer';
 
 
 CREATE OR REPLACE FUNCTION hyfaa.get_forecast_values_for_minibasin(mini integer, timeinterval varchar default '1 year')
