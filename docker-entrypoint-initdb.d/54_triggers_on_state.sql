d7cfde8f13e8f75a744105d69d9a428f51173b2b
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2021-05-19 19:08:10 +0200

    Improve trigger definition

--- docker-entrypoint-initdb.d/54_triggers_on_state.sql
+++ docker-entrypoint-initdb.d/54_triggers_on_state.sql
@@ -1,40 +1,30 @@
 -- Update the materialized views using triggers
 
-
---assimilated data
-CREATE OR REPLACE FUNCTION hyfaa.refresh_mat_view_for_assim()
+-- Post processing trigger
+CREATE OR REPLACE FUNCTION hyfaa.publication_post_processing()
     RETURNS TRIGGER LANGUAGE plpgsql
     SECURITY DEFINER
     AS $$
     BEGIN
---         REFRESH  MATERIALIZED VIEW hyfaa.data_assimilated_with_floating_avg_and_anomaly;
-        SELECT hyfaa.compute_expected_and_anomaly('hyfaa.data_assimilated', 'flow_median', 10);
-        REFRESH  MATERIALIZED VIEW hyfaa.data_with_assim_aggregate_geo;
-        RETURN null;
-    END $$;
+        -- mgbstandard data
+        IF NEW."tablename" = 'data_mgbstandard' THEN
+            RAISE INFO 'Triggering post-processing on mgbstandard table. Please wait...';
+            PERFORM hyfaa.compute_expected_and_anomaly('hyfaa.data_mgbstandard', 'flow_mean', 10);
+            REFRESH  MATERIALIZED VIEW hyfaa.data_with_mgbstandard_aggregate_geo;
+        END IF;
 
-DROP TRIGGER IF EXISTS refresh_mat_view_for_assim ON hyfaa."state";
-CREATE TRIGGER refresh_mat_view_for_assim
-    AFTER INSERT OR UPDATE ON hyfaa."state"
-    FOR EACH STATEMENT
-    EXECUTE PROCEDURE hyfaa.refresh_mat_view_for_assim();
+        -- assimilated data
+        IF NEW."tablename" = 'data_assimilated' THEN
+            RAISE INFO 'Triggering post-processing on assimilated table. Please wait...';
+            PERFORM hyfaa.compute_expected_and_anomaly('hyfaa.data_assimilated', 'flow_median', 10);
+            REFRESH  MATERIALIZED VIEW hyfaa.data_with_assim_aggregate_geo;
+        END IF;
 
-
--- mgbstandard data
-CREATE OR REPLACE FUNCTION hyfaa.refresh_mat_view_for_mgbstandard()
-    RETURNS TRIGGER LANGUAGE plpgsql
-    SECURITY DEFINER
-    AS $$
-    BEGIN
---         REFRESH  MATERIALIZED VIEW hyfaa.data_mgbstandard_with_floating_avg_and_anomaly;
-        SELECT hyfaa.compute_expected_and_anomaly('hyfaa.data_mgbstandard', 'flow_mean', 10);
-        REFRESH  MATERIALIZED VIEW hyfaa.data_with_mgbstandard_aggregate_geo;
         RETURN null;
     END $$;
 
 
-DROP TRIGGER IF EXISTS refresh_mat_view_for_mgbstandard ON hyfaa."state";
-CREATE TRIGGER refresh_mat_view_for_mgbstandard
+CREATE TRIGGER publication_post_processing
     AFTER INSERT OR UPDATE ON hyfaa."state"
-    FOR EACH STATEMENT
-    EXECUTE PROCEDURE hyfaa.refresh_mat_view_for_mgbstandard();
+    FOR EACH ROW
+    EXECUTE PROCEDURE hyfaa.publication_post_processing();
